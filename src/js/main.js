5714e5b61c2371d1a030789c20ce562c03544a32
Nick <deaneygaming@gmail.com>
2023-12-27 18:03:51 +0200

    small updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,65 @@
-import '../scss/style.scss';
+import '../scss/style.scss'
 
 // Scroll on click for navigation menu
-const menuLinks = document.querySelectorAll('.header__link[data-goto]');
+const menuLinks = document.querySelectorAll('.header__link[data-goto]')
 if (menuLinks.length > 0) {
-  menuLinks.forEach((menuLink) => {
-    menuLink.addEventListener('click', onMenuLinkClick);
-  });
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener('click', onMenuLinkClick)
+	})
 
-  function onMenuLinkClick(e) {
-    const menuLink = e.target;
-    if (
-      menuLink.dataset.goto &&
-      document.querySelector(menuLink.dataset.goto)
-    ) {
-      const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      const gotoBlockValue =
-        gotoBlock.getBoundingClientRect().top +
-        window.scrollY -
-        document.querySelector('header').offsetHeight;
+	function onMenuLinkClick(e) {
+		const menuLink = e.target
+		if (
+			menuLink.dataset.goto &&
+			document.querySelector(menuLink.dataset.goto)
+		) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto)
+			const gotoBlockValue =
+				gotoBlock.getBoundingClientRect().top +
+				window.scrollY -
+				document.querySelector('header').offsetHeight
 
-      window.scrollTo({
-        top: gotoBlockValue,
-        behavior: 'smooth',
-      });
-      e.preventDefault();
-    }
-  }
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: 'smooth',
+			})
+			e.preventDefault()
+		}
+	}
 }
 
 // Logo scroll
 document.addEventListener('DOMContentLoaded', () => {
-  const logoLink = document.querySelector('.header__logo-link');
-  const targetSection = document.getElementById('main');
-  const headerHeight = document.querySelector('header').offsetHeight;
+	const logoLink = document.querySelector('.header__logo-link')
+	const targetSection = document.getElementById('main')
+	const headerHeight = document.querySelector('header').offsetHeight
 
-  logoLink.addEventListener('click', (e) => {
-    e.preventDefault();
+	logoLink.addEventListener('click', e => {
+		e.preventDefault()
 
-    const targetOffset = targetSection.offsetTop - headerHeight;
+		const targetOffset = targetSection.offsetTop - headerHeight
 
-    window.scrollTo({
-      top: targetOffset,
-      behavior: 'smooth',
-    });
-  });
-});
+		window.scrollTo({
+			top: targetOffset,
+			behavior: 'smooth',
+		})
+	})
+})
 
 // Skincare arrow scroll
 document.addEventListener('DOMContentLoaded', () => {
-  const scrollTrigger = document.querySelector('.skincare__link');
-  const targetSection = document.getElementById('cosmetics');
-  const headerHeight = document.querySelector('header').offsetHeight;
+	const scrollTrigger = document.querySelector('.skincare__link')
+	const targetSection = document.getElementById('cosmetics')
+	const headerHeight = document.querySelector('header').offsetHeight
 
-  scrollTrigger.addEventListener('click', (e) => {
-    e.preventDefault();
+	scrollTrigger.addEventListener('click', e => {
+		e.preventDefault()
 
-    const targetOffset = targetSection.offsetTop - headerHeight;
+		const targetOffset = targetSection.offsetTop - headerHeight
 
-    window.scrollTo({
-      top: targetOffset,
-      behavior: 'smooth',
-    });
-  });
-});
+		window.scrollTo({
+			top: targetOffset,
+			behavior: 'smooth',
+		})
+	})
+})
