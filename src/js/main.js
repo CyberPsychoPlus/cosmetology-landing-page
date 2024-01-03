f30ed306ad381ef05b5d56a10c2372d58c27b9df
Nick <deaneygaming@gmail.com>
2024-01-03 12:58:59 +0200

    Added an animated scroll

--- src/js/main.js
+++ src/js/main.js
@@ -80,3 +80,19 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	})
 })
+
+//Animated scroll
+
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		console.log(entry)
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show')
+		} else {
+			entry.target.classList.remove('show')
+		}
+	})
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach(el => observer.observe(el))
